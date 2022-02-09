957f3f6a5cc0b2d85cf1908807a5bfd7efc7dde6
S-jooyoung <jooyoung.dev@gmail.com>
2022-02-09 20:00:00 +0000

    Feat:add login component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import Footer from "../footer/footer";
 import Header from "../header/header";
+import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
   const OnLogin = (event) => {
     authService.login(event.currentTarget.textContent).then(console.log);
   };
   return (
-    <section>
+    <section className={styles.login}>
       <Header />
       <section>
         <h1>Login</h1>
-        <ul>
-          <li>
-            <button onClick={OnLogin}>Google</button>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={OnLogin}>
+              Google
+            </button>
           </li>
-          <li>
-            <button onClick={OnLogin}>Github</button>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={OnLogin}>
+              Github
+            </button>
           </li>
         </ul>
       </section>
