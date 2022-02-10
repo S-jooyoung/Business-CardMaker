0980e79a524c44e85a4733d058ac59658e1bd270
S-jooyoung <jooyoung.dev@gmail.com>
2022-02-10 20:00:00 +0000

    Feat:add login and logout functionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
-  const OnLogin = (event) => {
-    authService.login(event.currentTarget.textContent).then(console.log);
+  let navigate = useNavigate();
+
+  const goToMaker = (userId) => {
+    navigate("/maker", { state: { id: userId } });
   };
+
+  const OnLogin = (event) => {
+    authService
+      .login(event.currentTarget.textContent) //
+      .then((data) => goToMaker(data.user.uid));
+  };
+
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        user && goToMaker(user.id);
+      });
+  });
   return (
     <section className={styles.login}>
       <Header />
