23aa317d6b01136d56a22a91c043b0d9a40bb47b
S-jooyoung <jooyoung.dev@gmail.com>
2022-02-16 20:00:00 +0000

    Feat:FileUnput Dependency Injection

--- src/components/cardmaker/cardmaker.jsx
+++ src/components/cardmaker/cardmaker.jsx
@@ -3,18 +3,19 @@ import CardAddForm from "../card_add_form/card_add_form";
 import CardEditForm from "../card_edit_form/card_edit_form";
 import styles from "./cardmaker.module.css";
 
-const CardMaker = ({ cards, addCard, updateCard, deleteCard }) => (
+const CardMaker = ({ FileInput, cards, addCard, updateCard, deleteCard }) => (
   <section className={styles.CardMaker}>
     <h1 className={styles.title}>CardMaker</h1>
     {Object.keys(cards).map((key) => (
       <CardEditForm
         key={key}
+        FileInput={FileInput}
         card={cards[key]}
         updateCard={updateCard}
         deleteCard={deleteCard}
       />
     ))}
-    <CardAddForm onAdd={addCard} />
+    <CardAddForm FileInput={FileInput} onAdd={addCard} />
   </section>
 );
 
