2b8892a3bfd99f6d310e6953e7d7eb0b58969fce
S-jooyoung <jooyoung.dev@gmail.com>
2022-07-02 17:39:20 +0900

    Test: headerComponent 컴포넌트 테스트

--- src/components/header/tests/header.test.js
+++ src/components/header/tests/header.test.js
@@ -1,11 +1,13 @@
 // import dependencies
 import React from "react";
+import userEvent from "@testing-library/user-event";
 
 // import custom-component
 import Header from "../header";
 
 // import snapshot-testing methods
 import renderer from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
 
 describe("Header", () => {
   let headerComponent;
@@ -19,5 +21,20 @@ describe("Header", () => {
   it("render", () => {
     const component = renderer.create(headerComponent).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  describe("Button Click", () => {
+    let button;
+
+    beforeEach(() => {
+      render(headerComponent);
+      button = screen.getByText("Logout");
+    });
+
+    it('calls onLogout when "Logout" button clicked', () => {
+      userEvent.click(button);
+
+      expect(onLogout).toHaveBeenCalled();
+    });
   });
 });
